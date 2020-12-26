4a89f0c9adaee923acbb8228a1ca70aa06ee663d
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2020-12-25 16:58:28 -0800

    Time series for all messages per month

--- graphers/TimeSeriesGenerator.py
+++ graphers/TimeSeriesGenerator.py
@@ -1,4 +1,6 @@
-from datetime import datetime
+from datetime import datetime, date
+from dateutil.relativedelta import relativedelta
+import matplotlib.pyplot as plt
 from collections import Counter
 import os
 
@@ -8,6 +10,8 @@ OUTPUT_FILE_FORMAT = ".png"
 
 # JSON field names
 MESSAGE_CONTENT = "content"
+TIMESTAMP_MS = "timestamp_ms"
+SENDER_NAME = "sender_name"
 
 
 class TimeSeriesGenerator:
@@ -22,12 +26,53 @@ class TimeSeriesGenerator:
 
         # Time series:
         # - Time series of messages (bucket size = 1 month? window-size averaging?)
-        # - Time series by person volume
-        #     - Time series by person % of bucket
-        # - Time series of video calls made, since the chat started
+        # - Time series by person % of bucket
 
-        self.__plot_time_series("TODO", messages)
+        self.__plot_message_volume(messages)
+        self.__plot_person_percentage_volume(messages)
 
 
-    def __plot_time_series(self, filename, messages):
+    def __plot_message_volume(self, messages):
+        def time_bucket(time):
+            # Hash together all messages in the same month
+            message_date = datetime.fromtimestamp(time/1000).date()
+            return date(year=message_date.year, month=message_date.month, day=1)
+        times = [time_bucket(m[TIMESTAMP_MS]) for m in messages]
+        counts = Counter(times).most_common()
+        
+        first_bucket, last_bucket = min(times), max(times)
+        def delta_months(month):
+            return (month.year - first_bucket.year) * 12 + (month.month - first_bucket.month)
+        num_month_buckets = delta_months(last_bucket) + 1
+        x_labels = [(first_bucket + relativedelta(months=i)).strftime("%B %Y") for i in range(num_month_buckets)]
+
+        x_indices = range(num_month_buckets)
+
+        values = [0] * num_month_buckets
+        for month, count in counts:
+            index = delta_months(month)
+            values[index] = count
+
+        figure, axes = plt.subplots()
+        figure.set_size_inches(9, 4)
+        axes.spines["right"].set_visible(False)
+        axes.spines["top"].set_visible(False)
+        
+        axes.set_title("# of Messages Per Month")
+        axes.set_ylabel("Messages Per Month")
+        axes.set_xticks(x_indices)
+        axes.set_xticklabels(x_labels, rotation=75)
+
+        axes.get_yaxis().set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
+
+        axes.plot(x_indices, values)
+
+        figure.tight_layout()
+        plt.savefig(self.output_directory_path + "messages_time_series" + OUTPUT_FILE_FORMAT, \
+            format="png", bbox_inches="tight")
+
+
+    def __plot_person_percentage_volume(self, messages):
+        #from IPython import embed; embed();
+        sys.exit(0) # TODO remove after done debugging
         pass
